bc8c6418d7f006f340c386c5a52a8fd2d4555acd
vnksnkr <vnksnkr@gmail.com>
2021-09-26 18:53:09 +0530

    format code

--- 8051/gesturectrl.c
+++ 8051/gesturectrl.c
@@ -1,16 +1,16 @@
-#include<reg51.h>
+#include <reg51.h>
 
-sbit opin = P2^0;
-sbit opin2 = P2^6;
-void delay(unsigned int delaycount){
+sbit pan_servo = P2 ^ 0;
+sbit tilt_servo = P2 ^ 6;
+void delay(unsigned int delaycount)
+{
 
-	unsigned int i;
-    for(i=0;i<(delaycount);i++){
+    unsigned int i;
+    for (i = 0; i < (delaycount); i++)
+    {
         ;
     }
-
 }
-
 
 // int servoctrl(char movptrection){
 
@@ -23,45 +23,39 @@ void delay(unsigned int delaycount){
 //     return 3;
 // }
 
-
 void main()
 {
 
-    
-    unsigned char pan=112,tilt=112;
-    char * movptr = &pan; 
-    
-    
+    unsigned char pan = 112, tilt = 112;
+    char *movptr = &pan;
+
     SCON = 0x50; // configure to 8 bit UART with variable baud rate
     TMOD = 0x20; // set timer 1 to 8 bit auto reload mode
-    TH1 = 0xfd; // set baud rate to 9600
+    TH1 = 0xfd;  // set baud rate to 9600
     TR1 = 1;
-    
 
-    while(1){
+    while (1)
+    {
 
         while (RI == 0) //wait for all 8 bits recieved
         {
-            opin = 1;
-			P1 = 0x01;
-            delay(pan+41);
-            opin = 0;
-			opin2 = 0x01;
-			delay(tilt+41);
-			opin2 = 0; 
-			delay(2160);
+            pan_servo = 1;
+            P1 = 0x01;
+            delay(pan + 41);
+            pan_servo = 0;
+            tilt_servo = 0x01;
+            delay(tilt + 41);
+            tilt_servo = 0;
+            delay(2160);
         }
 
-		P1 = 0x0;
+        P1 = 0x0;
         RI = 0;
-    
-        
 
         if (SBUF == 'p')
         {
             movptr = &pan;
         }
-        
 
         else if (SBUF == 't')
         {
@@ -72,8 +66,5 @@ void main()
         {
             *(movptr) = SBUF;
         }
-
-
     }
-
 }
